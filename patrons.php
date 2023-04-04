b61777ca35e41e495b29752af275f973025dd633
Sampath <kavilashtech@gmail.com>
2023-04-04 13:20:23 +0530

    changes as on 04-Apr-2023

---
+++ patrons.php
@@ -0,0 +1,7 @@
+<?php 
+    include 'includes/header.php'; 
+    include 'includes/menu.php'; 
+    include 'patrons.html'; 
+    include 'includes/footer.php'; 
+    include 'includes/script.php';
+?>
