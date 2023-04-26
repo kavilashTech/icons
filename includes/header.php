e6d8653141c7177bbdf6dc89fbd83fd69cc8e0c5
Sampath <kavilashtech@gmail.com>
2023-04-26 17:22:55 +0530

    added jquery reference

--- includes/header.php
+++ includes/header.php
@@ -15,6 +15,7 @@ session_start();
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- <link rel="stylesheet" type="text/css" href="js/rs-plugin/css/settings.css" media="screen"> -->
+  <script src="https://code.jquery.com/jquery-3.6.4.js" integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E=" crossorigin="anonymous"></script>
   <link rel="stylesheet" type="text/css" href="css/isotope.css" media="screen">
   <link rel="stylesheet" href="css/flexslider.css" type="text/css">
   <link rel="stylesheet" href="js/fancybox/jquery.fancybox.css" type="text/css" media="screen">
