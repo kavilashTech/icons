bf4de7bc44f27922d441eb4d1dcbe2adc9e80cd8
Sampath <kavilashtech@gmail.com>
2023-05-20 08:23:28 +0530

    venue page updates

--- venue.php
+++ venue.php
@@ -1,6 +1,6 @@
-<?php 
-    include 'includes/header.php'; 
-    include 'includes/menu.php'; 
+<?php
+include 'includes/header.php';
+include 'includes/menu.php';
 ?>
 <!-- Main Page Heading -->
 
@@ -14,16 +14,19 @@
           <h2 class="section-heading animated" data-animation="bounceInUp">Venue</h2>
         </div>
         <div class="">
+          <img src="img/confluence.jpg" style="float:right; margin-left:10px" class="responsive" width="50%" height="auto" alt="">
+        </div>
+        <p style="text-align:left">The conference will be held at Mamallapuram, Tamil Nadu, India. Mamallapuram (also called Mahabalipuram) is located about 60 Km south of Chennai city, which is well connected by international airlines from all over the world.<br>The International Airport at Chennai is served by taxis to Mamallapuram. Mamallapuram is an UNESCO world heritage site and is famous for traditional art & culture. The town is known for various historic monuments, particularly, monolithic rock carvings, built largely between 700 and 900 AD. Mamallapuram was also the venue for 44th International Chess Olympiad which was held in July-August, 2022 with over 2000 participants from 186 countries.</p>
+        <br>
+        <p style="text-align:left"> The climate will be salubrious with a high of 33°C and low of 27°C.</p>
+        <div class="">
           <img src="img/mahabs.jpg" style="float:right; margin-left:10px" class="responsive" width="50%" height="auto" alt="">
         </div>
-          <p style="text-align:left">The conference will be held at Grande Bay Resorts, Mamallapuram, Tamil Nadu, India Mamallapuram (also called Mahabalipuram) is located about 60 Km south of Chennai city, which is well connected by international airlines from all over the world.<br>The International Airport at Chennai is served by taxis to Mamallapuram. Mamallapuram is an UNESCO world heritage site and is famous for traditional art & culture. The town is known for various historic monuments, particularly, monolithic rock carvings, built largely between 700 and 900 AD. Mamallapuram was also the venue for 44th International Chess Olympiad which was held in July-August, 2022 with over 2000 participants from 186 countries.</p>
-          <br>
-          <p style="text-align:left"> The climate will be salubrious with a high of 33°C and low of 27°C.</p>
-        </div>
       </div>
-
-      <div class="col-md-1"></div>
     </div>
+
+    <div class="col-md-1"></div>
+  </div>
   </div>
 </section>
 
