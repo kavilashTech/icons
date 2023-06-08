9f6af9f12e98e5e581d73ac5715c02a49571011e
Sampath <kavilashtech@gmail.com>
2023-06-08 14:50:33 +0530

    Venue Page Updated

--- venue.php
+++ venue.php
@@ -17,17 +17,24 @@ include 'includes/menu.php';
         <div class="">
           <img src="img/confluence.jpg" style="float:right; margin-left:10px" class="responsive" width="50%" height="auto" alt="">
         </div>
-        <p style="text-align:left">The conference will be held at Mamallapuram, Tamil Nadu, India. Mamallapuram (also called Mahabalipuram) is located about 60 Km south of Chennai city, which is well connected by international airlines from all over the world.<br>The International Airport at Chennai is served by taxis to Mamallapuram. Mamallapuram is an UNESCO world heritage site and is famous for traditional art & culture. The town is known for various historic monuments, particularly, monolithic rock carvings, built largely between 700 and 900 AD. Mamallapuram was also the venue for 44th International Chess Olympiad which was held in July-August, 2022 with over 2000 participants from 186 countries.</p>
-        <br>
-        <p style="text-align:left"> The climate will be salubrious with a high of 33°C and low of 27°C.</p>
         <div class="">
           <img src="img/mahabs.jpg" style="float:right; margin-left:10px" class="responsive" width="50%" height="auto" alt="">
+        </div>
+        <p>The conference will be held at<b> Four Points by Sheraton- Mahabalipuram Resport and Convention Centre,</b> Mamallapuram, Tamil Nadu, India.</p>
+        <p style="text-align:left">Mamallapuram (also called Mahabalipuram) is located about 60 Km south of Chennai city, which is well connected by international airlines from all over the world.<br>The International Airport at Chennai is served by taxis to Mamallapuram. Mamallapuram is an UNESCO world heritage site and is famous for traditional art & culture. The town is known for various historic monuments, particularly, monolithic rock carvings, built largely between 700 and 900 AD. Mamallapuram was also the venue for 44th International Chess Olympiad which was held in July-August, 2022 with over 2000 participants from 186 countries.</p>
+        <br>
+        <p style="text-align:left">The venue is situated 12km away from the Indira Gandhi Centre for Atomic Research, an organizer of ICONS 2023.</p>
+        <p style="text-align:left"> During the conference, a pleasant climate is expected, with a high of 33°C and low of 27°C.</p>
+        <p style="text-align:left"> Please Scroll down for Map Link.</p>
+        <div class="">
+          <img src="img/hotel_front_view.jpg" style="float:right; margin-left:10px" class="responsive" width="50%" height="auto" alt="">
         </div>
         </div>
         <div class="row mar-top30">
         <div>
         <p style="text-align:center;font-size:18px;font-weight:bold;">Map of the Venue</p>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31147.129312771594!2d80.15217317630956!3d12.622324224067775!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a5254aa30074dc5%3A0x9d00999d9ca8933f!2sMahabalipuram%2C%20Tamil%20Nadu%20603104!5e0!3m2!1sen!2sin!4v1686057568748!5m2!1sen!2sin" width="100%" height="450" style="border:1;border-radius:5px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31147.129312771594!2d80.15217317630956!3d12.622324224067775!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a5254aa30074dc5%3A0x9d00999d9ca8933f!2sMahabalipuram%2C%20Tamil%20Nadu%20603104!5e0!3m2!1sen!2sin!4v1686057568748!5m2!1sen!2sin" width="100%" height="450" style="border:1;border-radius:5px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> -->
+          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3893.524816627313!2d80.167335!3d12.6135381!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a53ab48bafbfd25%3A0xbc3edeae6bb7ed46!2sFour%20Points%20by%20Sheraton%20Mahabalipuram%20Resort%20%26%20Convention%20Center!5e0!3m2!1sen!2sin!4v1686215513919!5m2!1sen!2sin" width="100%" height="450" style="border:1;border-radius:5px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
   </div>
         </div>
       </div>
